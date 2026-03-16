c6ccd19788f3a6260034696ff132b5161ecb1e69
manmay2 <imanmay2@gmail.com>
2026-03-16 18:27:36 +0530

    render bug fixed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/greet", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
